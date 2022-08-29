83079a4c8822c8418e3cb04a57bcfd9bb9120780
gayatri-innotech <gayatri@innotechteam.in>
2022-08-29 20:32:56 +0530

    Step-1 completed

---
+++ todo-react/src/components/todoList/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Todo = ({ todo, handleChange }) => {
+    console.log(todo)
+    const handleCheckboxChange = () => {
+        if (todo.done)
+            return null;
+        handleChange(todo.id)
+    }
+    return ( < div class = "box" >
+        <
+        input type = "checkbox"
+        style = {
+            { margin: "7px 1px 0 9px" }
+        }
+        checked = { todo.done }
+        onChange = {
+            () => { handleChange(todo.id) }
+        }
+        />   <span style = { { padding: "0 2px" }
+    } > { todo.title } < /span> <
+    span > { todo.done ? "completed" : "" } < /span>
+
+
+    <
+    /div>
+)
+}
+export default Todo
