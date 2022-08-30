a658dc21305959866962f5eeb8a97a447daf974d
gayatri-innotech <gayatri@innotechteam.in>
2022-08-30 11:36:50 +0530

    Step-1 changes

--- todo-react/src/components/todoList/index.js
+++ todo-react/src/components/todoList/index.js
@@ -8,24 +8,9 @@ const Todo = ({ todo, handleChange }) => {
         handleChange(todo.id)
     }
     return ( < div class = "box" >
-        <input type = "checkbox"
-        style = {
-            { margin: "7px 1px 0 9px" }
-        }
-        checked = { todo.done }
-        onChange = {
-            () => { handleChange(todo.id) }
-        }
-        />   <span style = { { padding: "0 2px" }
-    } > { todo.title } </span>  
-    <span style = {
-        {
-            margin: "0 0 0 1300px",
-            background: "lightgrey",
-            borderRadius: "25px",
-        }} > {todo.done ? "completed" : "" } </span>
-
-
+        <input type = "checkbox" style = {{ margin: "7px 1px 0 9px" }}checked = { todo.done }onChange = {() => { handleChange(todo.id) }}/>
+        <span style = { { padding: "0 2px" }} > { todo.title } </span>  
+    <span style = {{ margin: "0 0 0 1300px", background: "lightgrey", borderRadius: "25px",}} > {todo.done ? "completed" : "" } </span>
     </div>
 )
 }
